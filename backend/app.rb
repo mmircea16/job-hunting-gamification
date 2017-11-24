ec261322f370da5e530389a7a154bed53b0418af
Mircea M <mircea.moise@gmail.com>
2017-11-24 12:35:01 +0000

    Added mongodb connection

--- backend/app.rb
+++ backend/app.rb
@@ -1,5 +1,12 @@
 require 'sinatra'
+require 'mongo'
+
+PASSWORD=ENV['PASS']
+CONNECTION_STRING="mongodb://admin:#{PASSWORD}@jobhunting-shard-00-00-v8klp.mongodb.net:27017,jobhunting-shard-00-01-v8klp.mongodb.net:27017,jobhunting-shard-00-02-v8klp.mongodb.net:27017/test?ssl=true&replicaSet=JobHunting-shard-0&authSource=admin"
 
 get '/' do
+  client = Mongo::Client.new(CONNECTION_STRING)
+  db = client.database
+  puts db.collections.size
   'Hello world!'
 end
