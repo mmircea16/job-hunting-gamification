78cb942acb8954eea395f36a13b15a37c2090067
Mircea M <mircea.moise@gmail.com>
2017-11-24 14:40:36 +0000

    Added basic question

--- job-hunt/src/components/NameQuestion.jsx
+++ job-hunt/src/components/NameQuestion.jsx
@@ -1,12 +1,25 @@
-import React, { Component } from 'react';
-import {Card}  from 'reactbulma'
+import React, {Component} from 'react';
+import {Card, Button, Input, Field} from 'reactbulma'
 
 export class NameQuestion extends Component {
     render() {
-        return  <div className="App">
-            <Card>
-                <Card.Content>What's your name?</Card.Content>
-            </Card>
+        return <div className="App">
+            <div className="columns">
+                <div className="column is-one-third"/>
+                <div className="column">
+                    <Card>
+                        <Card.Content><div>What's your name?</div>
+                            <Field>
+                            <Input small id="small" />
+                            </Field>
+                            <Button primary>Continue</Button>
+                        </Card.Content>
+
+                    </Card>
+                </div>
+                <div className="column is-one-third"/>
+
+            </div>
         </div>
     }
 }
