13548ff56dc9b9aea8e2e712319a7a45721eb7c3
Mircea M <mircea.moise@gmail.com>
2017-11-24 13:58:11 +0000

    Added bulma

--- job-hunt/src/App.js
+++ job-hunt/src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button } from 'reactbulma'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome! I will help you get a job</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Before you start you need to read about how to get ready for employment. Once you've done that please go further
         </p>
+          <Button primary>Continue</Button>
       </div>
     );
   }
